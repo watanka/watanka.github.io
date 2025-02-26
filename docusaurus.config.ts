3ded47fa7db63e2293b56d93725ad1ae5651ceb1
eunsung <edplab2019@gmail.com>
2025-02-26 13:42:39 +0900

    adsense 추가

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -97,6 +97,10 @@ const config: Config = {
     // Replace with your project's social card
     metadata: [
       {
+        name: 'google-adsense-account',
+        content: 'ca-pub-3555505602575174'
+      },
+      {
         name: 'keywords',
         content: 'tech blog, 기술 블로그, AI 개발자, ML, 딥러닝, 백엔드',
       },
