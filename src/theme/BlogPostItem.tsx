0e0cac39c3121e6698e7f435b1b4c7ba1d703654
eunsung <eunsung.shin@gmail.com>
2025-03-19 09:16:06 +0900

    adsense 광고 추가

--- src/theme/BlogPostItem.tsx
+++ src/theme/BlogPostItem.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import BlogPostItem from "@theme-original/BlogPostItem";
 import Comment from "../components/Comment";
-import Adsense from '@site/src/components/Adsense';
+import Adsense from '../components/AdSense';
 
 export default function BlogPostItemWrapper(props) {
   return (
