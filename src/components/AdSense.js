de2232bdd2eeee166e7c8ba7914e2ed9d5773232
eunsung <eunsung.shin@gmail.com>
2025-03-19 09:27:38 +0900

    adsense 광고 추가

--- src/components/AdSense.js
+++ src/components/AdSense.js
@@ -1,15 +1,26 @@
-import React, { useEffect } from 'react';
+// src/components/AdSense.tsx
+import React, { useEffect, useRef } from 'react';
+import { useLocation } from '@docusaurus/router';
 
 export default function AdSense() {
+  const elementRef = useRef(null);
+  const location = useLocation();
+
   useEffect(() => {
-    if (window.adsbygoogle) {
-      window.adsbygoogle.push({});
+    try {
+      const adsbygoogle = window.adsbygoogle;
+      if (adsbygoogle && elementRef.current) {
+        adsbygoogle.push({});
+      }
+    } catch (err) {
+      console.error('AdSense 에러:', err);
     }
-  }, []);
+  }, [location]); // 페이지 변경시마다 광고 새로고침
 
   return (
-    <>
+    <div className="ad-container">
       <ins
+        ref={elementRef}
         className="adsbygoogle"
         style={{ display: 'block', textAlign: 'center' }}
         data-ad-layout="in-article"
@@ -17,6 +28,6 @@ export default function AdSense() {
         data-ad-client="ca-pub-3555505602575174"
         data-ad-slot="9587027628"
       />
-    </>
+    </div>
   );
 }
--- src/theme/BlogPostItem.tsx
+++ src/theme/BlogPostItem.tsx
@@ -2,12 +2,18 @@ import React from "react";
 import BlogPostItem from "@theme-original/BlogPostItem";
 import Comment from "../components/Comment";
 import Adsense from '../components/AdSense';
+import BrowserOnly from "@docusaurus/BrowserOnly";
+import { useLocation } from '@docusaurus/router';
 
 export default function BlogPostItemWrapper(props) {
+  const location = useLocation();
+
   return (
     <>
       <BlogPostItem {...props} />
-      <Adsense />
+      <BrowserOnly>
+        {() => <Adsense key={location.pathname} />}
+      </BrowserOnly>
       <Comment />
     </>
   );
